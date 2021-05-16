11b0a22078c9081d8ece61214a8526c7af8f395b
Vivek Singh Thakur <dev.svivek@gmail.com>
2021-05-17 02:57:21 +0530

    cumsum.js transpiling issue fixed with babel7

--- src/cumsum.js
+++ src/cumsum.js
@@ -1,6 +1,9 @@
 export default function cumsum(values, valueof) {
-  var sum = 0, index = 0;
-  return Float64Array.from(values, valueof === undefined
-    ? v => (sum += +v || 0)
-    : v => (sum += +valueof(v, index++, values) || 0));
+  var sum = 0,
+      index = 0;
+  return Float64Array.from(values, valueof === undefined ? function (v) {
+    return sum += +v || 0;
+  } : function (v) {
+    return sum += +valueof(v, index++, values) || 0;
+  });
 }
